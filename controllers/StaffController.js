8e5e8c29e40790d2c9be90892293afae23cf0c64
alabi sodiq <adisco4420@gmail.com>
2019-11-18 23:57:18 +0100

    upload image and update for staff

--- controllers/StaffController.js
+++ controllers/StaffController.js
@@ -1,5 +1,4 @@
 const StaffModel = require('../models/StaffModel');
-
 class StaffController {
    async Create(req, res) {
        try {
@@ -21,6 +20,55 @@ class StaffController {
             });
           }
        }
+    }
+    async UploadImg(req, res) {
+        try {
+            const id = req.staffId;
+            const updatedStaff = await StaffModel.findByIdAndUpdate(id, {
+                profilePic: req.imageUrl
+            }, {new: true})
+            res.status(200).json({status: 'success', data: updatedStaff});
+        } catch (error) {
+            res.status(500).json({status: 'error', msg: 'server error occured'})
+        }
+    }
+    async Update(req, res) {
+        try {
+            const user = await StaffModel.findByIdAndUpdate(req.params.id, {
+                ...req.body
+            }, {new: true})
+            if (!user) return res.status(404).json({
+              status: 'bad input',
+              message: 'user not found'
+            });
+            res.status(200).json({
+              status: 'success',
+              data: user
+            })
+          } catch (error) {
+            res.status(500).json({
+              status: 'error',
+              message: 'server error'
+            })
+          }
+    }
+    async View(req, res) {
+        try {
+            const user = await StaffModel.findById(req.params.id)
+            if (!user) return res.status(404).json({
+              status: 'bad input',
+              message: 'user not found'
+            });
+            res.status(200).json({
+              status: 'success',
+              data: user
+            })
+          } catch (error) {
+            res.status(500).json({
+              status: 'error',
+              message: 'server error'
+            })
+          }
     }
 }
 
--- env.js
+++ env.js
@@ -5,5 +5,9 @@ module.exports = {
     JWT_SECRET: process.env.JWT_SECRET,
     MONGODB_URI: process.env.MONGODB_URI,
     SENDGRID_KEY: process.env.SENDGRID_KEY,
-    BASEURL: process.env.BASEURL
+    BASEURL: process.env.BASEURL,
+    cloudinary: {
+        apiKey: process.env.CDYapiKey,
+        secretKey: process.env.CDYsecretKey
+    }
 }

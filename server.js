0b7293705021dbc4ed99f552409139a513cef94a
alabi sodiq <adisco4420@gmail.com>
2019-11-18 20:05:57 +0100

    user route model and controller

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const UserRoute = require('./routes/UserRoute');
+const port = process.env.PORT || 6004; 
+const app = express();
+const env = require('./env');
+// Connect to MongoDB
+mongoose
+  .connect(env.MONGODB_URI, { useNewUrlParser: true })
+  .then(() => {
+    console.log('✌🏾 Successfully connected to MongoDB');
+  })
+  .catch(err => {
+    console.log('An error occured while conencting to MongoDB', err);
+  });
+
+app.use(cors());
+
+// Add middlewares for parsing JSON and urlencoded data and populating `req.body`
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.json());
+
+app.use('/user', UserRoute);
+
+app.listen(port).on('listening', () => {
+  console.log('We are live on ' + port);
+});
